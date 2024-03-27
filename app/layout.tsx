cd306d6602808857877117b869cbb208c06f4503
[Sahil khatiwada] <sahilkhatiwada12@gmail.com>
2024-03-28 01:41:31 +0545

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Portfolio",
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
+        <SpeedInsights />
         <main className="relative overflow-hidden">{children}</main>
         <Footer />
       </body>
